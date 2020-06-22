3e215bd0111b5c7acb7369630501a16e5c654706
Thabang soulo <tsoulo@c4r11s10.wethinkcode.co.za>
2020-06-22 15:15:26 +0200

    ex00

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: tsoulo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 13:12:33 by tsoulo            #+#    #+#             */
-/*   Updated: 2020/06/22 12:34:12 by tsoulo           ###   ########.fr       */
+/*   Updated: 2020/06/22 15:14:06 by tsoulo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int		ft_putchar(char c);
 void	ft_print_alphabet(void)
 {
 	     char alpha;	
-	     alpha  = 'z';
+	     alpha  = 'a';
 
-         while	(alpha >= 'a')
+         while	(alpha >= 'b')
 	     {
 	 		 ft_putchar(alpha);
-	 		 alpha--;
+	 		 alpha++;
          }
 	     ft_putchar('\n');
 }
