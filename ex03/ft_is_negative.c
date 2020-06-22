58baf0eca7fa485b3c2a1406ef51be711f4ce45d
Thabang soulo <tsoulo@c4r11s10.wethinkcode.co.za>
2020-06-22 12:55:57 +0200

    ex03

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,28 +6,23 @@
 /*   By: tsoulo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 14:39:58 by tsoulo            #+#    #+#             */
-/*   Updated: 2020/06/17 15:36:09 by tsoulo           ###   ########.fr       */
+/*   Updated: 2020/06/22 12:38:05 by tsoulo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int		ft_putchar(char c);
 
-int ft_putchar(char c){
-    write(1,&c,1);
-	return (0);
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+    	ft_putchar('N');
+ 	}
+	else if (n >=0){
+   		 ft_putchar('P');
+	}
+	ft_putchar('\n');
 }
-void ft_is_negative(int n){
 
- if(n < 0){
-  ft_putchar('N');
- }else if(n >=0){
-  ft_putchar('P');
-  }
- ft_putchar('\n');
-}
-
-int main(){
-
-  return(0); 
-}
