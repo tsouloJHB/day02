d146616159c0e7b218ebc0c65e3806da6155b9cc
Thabang soulo <tsoulo@c4r11s10.wethinkcode.co.za>
2020-06-22 12:46:55 +0200

    ex00

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,27 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsoulo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 13:12:33 by tsoulo            #+#    #+#             */
+/*   Updated: 2020/06/22 12:34:12 by tsoulo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
+int		ft_putchar(char c);
 
-int ft_putchar(char c){
+void	ft_print_alphabet(void)
+{
+	     char alpha;	
+	     alpha  = 'z';
 
-	write(1,&c,1);
-	return (0);
+         while	(alpha >= 'a')
+	     {
+	 		 ft_putchar(alpha);
+	 		 alpha--;
+         }
+	     ft_putchar('\n');
 }
 
-void ft_print_alphabet(void){
 
-   char alpha = 'a';
-   while( alpha <= 'z'){
-	   ft_putchar(alpha);
-	   alpha++;
-   }
-
-
-}
-
-int main(){
-
-  ft_print_alphabet();
-  return (0);
-
-}
 		
