3f837d0df13c7befe5200349a37ccbd072b69351
Thabang soulo <tsoulo@c4r11s10.wethinkcode.co.za>
2020-06-22 12:55:16 +0200

    ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,40 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsoulo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/17 14:25:54 by tsoulo            #+#    #+#             */
-/*   Updated: 2020/06/17 14:26:05 by tsoulo           ###   ########.fr       */
+/*   Created: 2020/06/22 10:03:02 by tsoulo            #+#    #+#             */
+/*   Updated: 2020/06/22 12:35:12 by tsoulo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-
 #include <unistd.h>
 
-int ft_putchar(char c){
+int 	ft_putchar(char c);
 
- write(1,&c,1);
- return (0);
-}
+void	ft_print_reverse_alphabet(void)
+{
+	char alpha;
 
-void ft_print_numbers(void){
-
-  int num = '1';
-  char ls;
-  while(num <= '9'){
-	  ft_putchar(num);
-	  num++;
-  }
-}
-
-int main(){
-
-ft_print_numbers();
-ft_putchar('\n');
-return(0);
-
+	alpha  = 'z';
+	while (alpha  >= 'a')
+	{
+		ft_putchar(alpha);
+		alpha--;
+	}
+	ft_putchar('\n');
 }
