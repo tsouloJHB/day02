10221e27a2661600bac8bcb1b9a644db25831afa
Thabang soulo <tsoulo@c4r11s10.wethinkcode.co.za>
2020-06-23 14:23:11 +0200

    ex04

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: tsoulo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 16:02:09 by tsoulo            #+#    #+#             */
-/*   Updated: 2020/06/22 12:39:16 by tsoulo           ###   ########.fr       */
+/*   Updated: 2020/06/23 14:14:24 by tsoulo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	ft_print_comb(void)
 			{
 				if (k != li && k != ls)
 				{
+				   write(1," ",1);	
                    ft_putchar(li);
 				   ft_putchar(ls);
 				   ft_putchar(k);
